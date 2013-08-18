218b233e7cd4470df9907659e495ce7e6f9289bc
Randall Lybbert <rlybbert@gmail.com>
2013-08-17 20:33:05 -0600

    Trying link on picture to google

--- www/js/controls.js
+++ www/js/controls.js
@@ -1,6 +1,7 @@
 var curPage = 1;
 var totalPages = 0;
-
+var pageHeight = 0;
+var totalSpeed = 4000;
 $(function() {
     
 	totalPages = $('.page').size();
@@ -18,16 +19,28 @@ $(function() {
 
 function nextPage(){
 	if(curPage < totalPages){
+		getCurrentPage();
 		curPage++;
-
-		$('html, body').stop().animate({ scrollTop: (curPage * 1300) - 1300 + 138}, 4000);
+		var distanceToGo = Math.abs(pageHeight - (((curPage-1) * 1300) + 130));
+		var speed = (distanceToGo/1300) * totalSpeed;
+		$('html, body').stop().animate({ scrollTop: ((curPage-1) * 1300) + 130}, speed);
 	}
 }
 
 function prevPage(){
 	if(curPage > 1){
+		getCurrentPage();
 		curPage--;
+		var distanceToGo = Math.abs(pageHeight - (((curPage-1) * 1300) + 130));
+		var speed = (distanceToGo/1300) * totalSpeed;
+		$('html, body').stop().animate({ scrollTop: ((curPage-1) * 1300) + 130}, speed);
+	}
+}
 
-		$('html, body').stop().animate({ scrollTop: (curPage * 1300) - 1300 + 138}, 4000);
+function getCurrentPage(){
+	pageHeight = $('body').scrollTop(); 
+	curPage = Math.ceil(pageHeight / 1300);
+	if(curPage <= 0){
+		curPage = 1;
 	}
 }
